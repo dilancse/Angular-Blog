cec216a4ee3d3b9cb73080f62a168e13a467b343
Dilan Nuwantha <dilan.nuwantha@gmail.com>
2020-02-08 22:58:22 +0530

    Article routing and defalut routing added when incorrect url

--- Angular-Blog/src/app/Services/articles.service.ts
+++ Angular-Blog/src/app/Services/articles.service.ts
@@ -11,8 +11,13 @@ export class ArticlesService {
   constructor() { }
 
   getArticles(): Observable<Article[]> {
-    const article: Article[] = ARTICLES;
+    const articles: Article[] = ARTICLES;
 
-    return of(article);
+    return of(articles);
+  }
+
+  getArticle(key: string): Observable<Article> {
+    const article: Article[] = ARTICLES.filter(artic => artic.key === key);
+    return of(article[0]);
   }
 }
--- Angular-Blog/src/app/article/article.component.ts
+++ Angular-Blog/src/app/article/article.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArticlesService } from '../Services/articles.service';
 
 @Component({
   selector: 'app-article',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  article: Article = null;
+
+  constructor(private route: ActivatedRoute,
+              private articleService: ArticlesService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe( params => {
+      const key = params.key;
+      this.articleService.getArticle(key).subscribe(currentArticle => {
+        if (currentArticle === undefined || currentArticle === null) {
+          this.router.navigateByUrl('');
+          return;
+        }
+        this.article = currentArticle;
+      });
+    });
   }
 
 }
