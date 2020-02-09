4de29b3658c8e5e31892fd3dcf3036e3ee572eb6
Dilan Nuwantha <dilan.nuwantha@gmail.com>
2020-02-09 23:11:42 +0530

    http client to get data from api call

--- Angular-Blog/src/app/Services/articles.service.ts
+++ Angular-Blog/src/app/Services/articles.service.ts
@@ -2,22 +2,26 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Article } from '../models/article';
 import { ARTICLES } from '../models/mock-articles';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticlesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]> {
-    const articles: Article[] = ARTICLES;
+    // const articles: Article[] = ARTICLES;
 
-    return of(articles);
+    // return of(articles);
+    return this.http.get<Article[]>('http://localhost:8000/articles');
   }
 
   getArticle(key: string): Observable<Article> {
-    const article: Article[] = ARTICLES.filter(artic => artic.key === key);
-    return of(article[0]);
+    // const article: Article[] = ARTICLES.filter(artic => artic.key === key);
+    // return of(article[0]);
+
+    return this.http.get<Article>('http://localhost:8000/articles/' + key);
   }
 }
